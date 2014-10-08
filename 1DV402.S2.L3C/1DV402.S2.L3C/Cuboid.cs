61f4f0ef9e42adc1caf6eaed7a1c2589041210cd
Daniel Vedin <dv222bk@student.lnu.se>
2014-10-08 11:58:40 +0200

    Cuboid.cs completed

---
+++ 1DV402.S2.L3C/1DV402.S2.L3C/Cuboid.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S2.L3C
+{
+    public class Cuboid : Shape3D
+    {
+        public double MantelArea
+        {
+            get
+            {
+                return _baseShape.Perimeter * Height;
+            }
+        }
+        public double TotalSurfaceArea
+        {
+            get
+            {
+                return MantelArea + 2 * _baseShape.Area;
+            }
+        }
+        public double Volume
+        {
+            get
+            {
+                return _baseShape.Area * Height;
+            }
+        }
+        public Cuboid(double length, double width, double height) 
+            : base(ShapeType.Cubiod, new Rectangle(length, width), height ) { }
+    }
+}
